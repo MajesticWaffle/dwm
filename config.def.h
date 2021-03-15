65594b2ff6e933e0edef5101168176f6eeba985f
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2021-03-15 13:00:29 -0500

    getting fancy now

--- config.def.h
+++ config.def.h
@@ -2,9 +2,17 @@
 
 #include <X11/XF86keysym.h>
 
+
+/* Im small brain so this makes it easy */
+#define tag_gen 	1
+#define tag_code 	1 << 1
+#define tag_disc 	1 << 2
+#define tag_steam 	1 << 3
+#define tag_lock	1 << 4
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -12,7 +20,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10" };
+static const char *fonts[]          = { "Source Code Pro:size=10", "Font Awesome:Size=12" };
 static const char dmenufont[]       = "Source Code Pro:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -21,21 +29,26 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#1C71D8";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray1  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2 },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4" };
+static const char *tags[] = { "", "", "", "", "" };		//General (FF, term, etc), Coding, Discord, Steam, Lock
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class  		instance	title     	tags mask   	isfloating	monitor */
+	
+	{ "code-oss",		NULL,		NULL,		tag_code,	0,		-1 },
+	{ "jetbrains-clion",	NULL,		NULL,		tag_code,	0,		-1 },
+	{ "jetbrains-idea",	NULL,		NULL,		tag_code,	0,		-1 },
+
+	{ "Steam",		NULL,		NULL,		tag_steam, 	0,		-1 },
+	{ "discord",		NULL,		NULL,		tag_disc, 	0,		-1 },
 };
 
 /* layout(s) */
@@ -66,25 +79,24 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "fish", NULL };	//Weird hack because *shell is ignored in st config.h??
+static const char *termcmd[]  = { "st", "fish", NULL };
 
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *upvol[]   = { "volinc", NULL };
+static const char *downvol[] = { "voldec", NULL };
+static const char *mutevol[] = { "volmut", NULL };
 
-static const char *scrcmd[] = { "xfce4-screenshooter", "-c", "-r", NULL};
-static const char *tskmgr[] = { "xfce4-taskmanager", NULL};
+static const char *scrcmd[] = { "scrshot", NULL};
+
 static Key keys[] = {
 	/* Volume Keys */
-	{0,	XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{0,	XF86XK_AudioMute, spawn, {.v = mutevol } },
-	{0,	XF86XK_AudioRaiseVolume, spawn, {.v = upvol } },
-
+	{0,				XF86XK_AudioLowerVolume, 	spawn, {.v = downvol } },
+	{0,				XF86XK_AudioMute, 		spawn, {.v = mutevol } },
+	{0,				XF86XK_AudioRaiseVolume, 	spawn, {.v = upvol } },
+	
 	/* Screenshot */
-	{ WINKEY|ShiftMask,		XK_s, spawn, {.v = scrcmd } },
+	{ WINKEY|ShiftMask,		XK_s, 				spawn, {.v = scrcmd } },
 
-	/* Task manager */
-	{ ControlMask | ShiftMask,      XK_Escape, spawn, {.v = tskmgr } },
+	/* Switch to lock tag */
 
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
--- dwmblocks-old/blocks.h
+++ dwmblocks-old/blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/dwm/scripts/dwmmem",			2,		0},
-	{"", "~/dwm/scripts/dwmnet",			1,		0},
-	{"", "~/dwm/scripts/dwmvol",			1,		0},
-	{"", "~/dwm/scripts/dwmdte",			30,		0},
+	{"", "dwmmem",			2,		0},
+	{"", "dwmnet",			1,		0},
+	{"", "dwmvol",			1,		0},
+	{"", "dwmdte",			30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
